c4cd78a87edc28228977482a275eb51adf2a9a47
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-08-09 15:00:43 -0500

    employee object properties created

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,10 @@
 const Employee = require('../lib/Emplyee');
+
+test('creates an employee object', () =>{
+    const employee = new Employee('Name');
+
+    expect(employee.name).toBe('Name');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.stringContaining(employee.email.toString()));
+});
+
