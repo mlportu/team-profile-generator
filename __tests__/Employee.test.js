ecda3b97981f8bb7e6e2518048aa14960019d8ad
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-08-09 15:31:55 -0500

    employee object created with methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -8,3 +8,26 @@ test('creates an employee object', () =>{
     expect(employee.email).toEqual(expect.stringContaining(employee.email.toString()));
 });
 
+test('gests employee name', () =>{
+    const employee = new Employee('Name');
+
+    expect(employee.getName()).toEqual(expect.stringContaining(employee.name));
+});
+
+test('gets employee id', () =>{
+    const employee = new Employee('Name');
+
+    expect(employee.getId()).toBeGreaterThan(0);
+});
+
+test('gets employee email', () =>{
+    const employee = new Employee('Name');
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email));
+});
+
+test('gets the designated role of the employee', ()=>{
+    const employee = new Employee('Name');
+
+    expect(employee.getRole()).toBe('Employee');
+});
