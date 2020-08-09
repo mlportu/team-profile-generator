de1e88a4af856a0b207a32a4f94e8355276a5d41
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-08-09 17:31:35 -0500

    user inputs mangager info

--- index.js
+++ index.js
@@ -1,12 +1,69 @@
+const inquirer = require('inquirer');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern')
 
-
-
-const loadEmployees = () =>{
-    console.log(Manager.name);
-    console.log(Manager.id);
+function developTeam (){
+    this.manager;
+    this.egineer;
+    this.intern;
+}
+developTeam.prototype.managerInfo = function(){
+    inquirer.prompt([
+        {
+            type:'input',
+            name:'name',
+            message:"What is your team manager's name?",
+            validate: nameInput =>{
+                if(nameInput){
+                    return true;
+                } else {
+                    console.log("Please enter your team manager's name");
+                    return false;
+                }
+            }
+        },
+        {
+            type:'input',
+            name:'id',
+            message:"What is your team manager's id?",
+            validate: idInput =>{
+                if(idInput){
+                    return true;
+                } else {
+                    console.log("Please enter your mangaer's id");
+                    return false;
+                }
+            }
+        },
+        {
+            type:'input',
+            name:'email',
+            message:"What is your team manager's email?",
+            validate: idInput =>{
+                if(idInput){
+                    return true;
+                } else {
+                    console.log("Please enter your mangaer's email");
+                    return false;
+                }
+            }
+        },
+        {
+            type:'input',
+            name:'officeNumber',
+            message:"What is your team manager's office number?",
+            validate: idInput =>{
+                if(idInput){
+                    return true;
+                } else {
+                    console.log("Please enter your mangaer's office number");
+                    return false;
+                }
+            }
+        } 
+    ])
 }
 
-loadEmployees();
+
+developTeam();
