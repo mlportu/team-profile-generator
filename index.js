3f1f81179c9cd21f0587ff56cb7cdf9aa6390a67
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-08-09 16:59:40 -0500

    test manager inheritance

---
+++ index.js
@@ -0,0 +1,12 @@
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern')
+
+
+
+const loadEmployees = () =>{
+    console.log(Manager.name);
+    console.log(Manager.id);
+}
+
+loadEmployees();
